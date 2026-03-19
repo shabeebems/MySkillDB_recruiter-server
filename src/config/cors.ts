b2eb816871921731799b6e96040cfd81baeebb0a
ameenhyder-v <ameenhyder.v@gmail.com>
2026-03-19 13:52:38 +0530

    changed cors

--- src/config/cors.ts
+++ src/config/cors.ts
@@ -2,7 +2,6 @@ import cors from "cors";
 
 export const corsMiddleware = cors({
   origin: function (origin, callback) {
-    // Always allow the origin to pass CORS
     callback(null, origin || true);
   },
   methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH"],
